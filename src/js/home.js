61b6d1b07b1fb4feaab95b07345f942aa8ccdc78
ndevare13 <ndevare13@gmail.com>
2021-05-08 18:00:01 +0530

    Fixed the name of restaurant on Home page

--- src/js/home.js
+++ src/js/home.js
@@ -5,7 +5,7 @@ const createHomePageElements = () => {
     <img id="main-img" src="https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80 " alt="delicious-looking-food ">
     </div>
     <div class="tagline">
-        <h2>Welcome to Sunrise Coast!</h2>
+        <h2>Welcome to Sunrise Roast!</h2>
         <p>"...where fine dining is an authentic experience..."</p>
     </div>`;
 
